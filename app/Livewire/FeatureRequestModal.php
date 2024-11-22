15affc2b8a4e2d8bcf3ef84fa71cb38bae561126
Johnson Sebire <johnson@manifestghana.com>
2024-11-22 07:44:17 +0000

    Added role to fillable fields for User model

---
+++ app/Livewire/FeatureRequestModal.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class FeatureRequestModal extends Component
+{
+    public function render()
+    {
+        return view('livewire.feature-request-modal');
+    }
+}
---
+++ app/Livewire/SupportRequestModal.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class SupportRequestModal extends Component
+{
+    public function render()
+    {
+        return view('livewire.support-request-modal');
+    }
+}
