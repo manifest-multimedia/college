d0f1cdd934b281e161030513d3cf61d4ead55696
Johnson Sebire <johnson@manifestghana.com>
2024-11-25 15:45:55 +0000

    Added CRSF Token to Header Files

--- resources/views/components/backend/layout.blade.php
+++ resources/views/components/backend/layout.blade.php
@@ -5,6 +5,7 @@
     <title>{{ config('app.name', 'Laravel') }} - {{ $title }}</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400&display=swap">
     <link rel="stylesheet" href="{{ asset('backend/css/bootstrap/bootstrap.min.css') }}">
