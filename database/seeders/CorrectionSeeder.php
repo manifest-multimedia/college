72f00fc44a037b36d9a9bd5d2ec79134c6030051
Johnson Sebire <johnson@manifestghana.com>
2024-11-27 20:29:59 +0000

    Updated CorrectionSeeder

--- database/seeders/CorrectionSeeder.php
+++ database/seeders/CorrectionSeeder.php
@@ -15,62 +15,49 @@ class CorrectionSeeder extends Seeder
     public function run()
     {
         // Dataset Location: public/datasets/students.csv
+        $student_id = "PNMTC/DA/RGN/22/23/218";
+        $first_name = "Leticia";
+        $last_name = "Agyei";
+        $other_names = "Abankwa";
+        $gender = "female";
+        $date_of_birth = "1998-11-01";
+        $country_of_residence = "GHANA";
+        $marital_status = "single";
+        $home_region = "Bono";
+        $home_town = "Dormaa Ahenkro";
+        $religion = "Christian";
+        $mobile_number = "024 123 4567";
+        $email = "letabankwa1@gmail.com";
+        $postal_address = "POST OFFICE BOX 4";
+        $gps_address = "BD-0004-9981";
+        $residential_address = "SM 27,DORMAA AHENKRO";
+        $class = "Registered General Nursing";
 
-        // $path = storage_path('app/public/datasets/correct_data.csv');
-        $path = public_path('datasets/leticia.xlsx');
-        // $handle = fopen('storage/app/public/datasets/correct_data.csv', 'r');
-
-        $handle = fopen($path, 'r');
-
-        // Get the column names from the first row
-        $columnNames = fgetcsv($handle);
-
-        // Initialize an empty array to store the data
-        $data = [];
-
-        // Iterate over the remaining rows
-        while (($row = fgetcsv($handle)) !== FALSE) {
-            // Combine the column names with the row data to create an associative array
-            $rowData = array_combine($columnNames, $row);
-
-            // Now you can access the data using the column names
-            echo $rowData['first_name'] . "\n";
-
-            // You can also use the data to create a new student
-            // $studentId = generateStudentID(null, $rowData['class']);
-            // $existingStudent = Student::where('student_id', $studentId)->first();
-
-
-            $student = Student::firstOrCreate([
-                'student_id' => $rowData['index_number'] ? $rowData['index_number'] : null,
-                'first_name' => ucfirst(strtolower($rowData['first_name'])),
-                'last_name' => ucfirst(strtolower($rowData['last_name'])),
-                'other_name' => ucfirst(strtolower($rowData['other_names'])),
-                'gender' => ucfirst(strtolower($rowData['gender'])),
-                'date_of_birth' => ucfirst(strtolower($rowData['date_of_birth'])),
-                'nationality' => ucfirst(strtolower($rowData['nationality'])),
-                'marital_status' => ucfirst(strtolower($rowData['marital_status'])),
-                'country_of_residence' => ucfirst(strtolower($rowData['country_of_residence'])),
-                'home_region' => ucfirst(strtolower($rowData['home_region'])),
-                'home_town' => ucfirst(strtolower($rowData['home_town'])),
-                'religion' => ucfirst(strtolower($rowData['religion'])),
-                'mobile_number' => $rowData['mobile_number'],
-                'email' => strtolower($rowData['email']),
-                'postal_address' => ucfirst(strtolower($rowData['postal_address'])),
-                'residential_address' => ucfirst(strtolower($rowData['residential_address'])),
-                'gps_address' => 'BD-0003-5130',
-            ]);
-            $class = CollegeClass::firstOrCreate([
-                'name' => $rowData['class'],
-                'slug' => strtolower($rowData['class']),
-            ]);
-            // Associate the student with the class
-            $student->collegeClass()->associate($class);
-            $student->save();
-        }
-
-        // Close the file
-
-        fclose($handle);
+        $student = Student::firstOrCreate([
+            'student_id' => ucfirst(strtolower($student_id)),
+            'first_name' => ucfirst(strtolower($first_name)),
+            'last_name' => ucfirst(strtolower($last_name)),
+            'other_name' => ucfirst(strtolower($other_names)),
+            'gender' => ucfirst(strtolower($gender)),
+            'date_of_birth' => ucfirst(strtolower($date_of_birth)),
+            'nationality' => ucfirst(strtolower($country_of_residence)),
+            'marital_status' => ucfirst(strtolower($marital_status)),
+            'country_of_residence' => ucfirst(strtolower($country_of_residence)),
+            'home_region' => ucfirst(strtolower($home_region)),
+            'home_town' => ucfirst(strtolower($home_town)),
+            'religion' => ucfirst(strtolower($religion)),
+            'mobile_number' => $mobile_number,
+            'email' => strtolower($email),
+            'postal_address' => ucfirst(strtolower($postal_address)),
+            'residential_address' => ucfirst(strtolower($residential_address)),
+            'gps_address' => $gps_address,
+        ]);
+        $class = CollegeClass::firstOrCreate([
+            'name' => $class,
+            'slug' => strtolower($class),
+        ]);
+        // Associate the student with the class
+        $student->collegeClass()->associate($class);
+        $student->save();
     }
 }
