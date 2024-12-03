82998c83441613f57c725673e0bd5dea708c849f
Johnson Sebire <johnson@manifestghana.com>
2024-12-03 16:34:14 +0000

    Added Better Check for Env

--- resources/views/livewire/data-mismatch.blade.php
+++ resources/views/livewire/data-mismatch.blade.php
@@ -22,9 +22,10 @@
                 Records for Leticia, RGN Class 
                 
             </h1>
+            @if (env('APP_ENV') == 'production')
             <p>
                 {{-- Check if Env is Production --}}
-                @if(env('APP_ENV') === 'production')
+              
                     <span class="badge bg-success">Production</span>
                 @if(\App\Models\ExamSession::where('student_id', 131)->count() > 0)
                 @foreach (\App\Models\ExamSession::where('student_id', 131)->get() as $sessions)
@@ -37,22 +38,28 @@
                     
                 @endforeach
                 @endif
-                @else
-                    <span class="badge bg-warning">Local</span>
-                    {{-- ID on Local is 90 --}}
-                    @if(\App\Models\ExamSession::where('student_id', 90)->count() > 0)
-                    @foreach (\App\Models\ExamSession::where('student_id', 90)->get() as $sessions)
-                {{ \App\Models\User::where('id', 90)->first()->name }} <br>   
-                    {{-- {{ $sessions->exam->exam_code }} <br> --}}
-                        Course: {{ $sessions->exam->course->name }} <br>
-                        Responses: {{ $sessions->responses->count() }} <br>
-                       Session ID: {{ $sessions->id }} <br>
-                        <hr>
-                        
-                    @endforeach
-                    @endif
-                @endif 
+               
+               
             </p>
+            @endif
+            @if (env('APP_ENV') == 'local')
+    
+            <p>     <span class="badge bg-warning">Local</span>
+                {{-- ID on Local is 90 --}}
+                @if(\App\Models\ExamSession::where('student_id', 90)->count() > 0)
+                @foreach (\App\Models\ExamSession::where('student_id', 90)->get() as $sessions)
+            {{ \App\Models\User::where('id', 90)->first()->name }} <br>   
+                {{-- {{ $sessions->exam->exam_code }} <br> --}}
+                    Course: {{ $sessions->exam->course->name }} <br>
+                    Responses: {{ $sessions->responses->count() }} <br>
+                   Session ID: {{ $sessions->id }} <br>
+                    <hr>
+                    
+                @endforeach
+                @endif
+        </p>
+@endif
+            
         </div>
 
 
