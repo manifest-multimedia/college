cfd173d30e9414bf656abaae064f4baf75ba5ea8
Johnson Sebire <johnson@manifestghana.com>
2026-05-14 12:49:59 +0000

    Enabled Elibgility Check for Exam Login

--- app/Livewire/ExamLogin.php
+++ app/Livewire/ExamLogin.php
@@ -66,13 +66,14 @@ class ExamLogin extends Component
             return;
         }
 
-        // Additional validation: Check if the student is eligible to take the exam
-        // if (!$student->isEligibleForExam()) {
-        //     session()->flash('error', "
-        //     Dear " . $student->first_name . ",
-        //     You are not eligible to take this exam. You have pending fees to clear. Please see the accounts Office for clearance.");
-        //     return;
-        // }
+       // Additional validation: Check if the student is eligible to take the exam
+
+        if (!$student->isEligibleForExam() && config('app.name') == 'MHTIA CIS') {
+            session()->flash('error', "
+            Dear " . $student->first_name . ",
+            You are not eligible to take this exam. You have pending fees to clear. Please see the accounts Office for clearance.");
+            return;
+        }
 
         // Create or find User for Student
         try {
